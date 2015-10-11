3de02f85a7cf1b4033aa1e99c0047d15825fb231
RoRElessar <palantiri@ukr.net>
2015-10-12 00:57:04 +0300

    done lesson 4 & edit lesson 1

--- 1.rb
+++ 1.rb
@@ -1,6 +1,3 @@
 puts 'Enter a number'
 number = gets.chomp
-puts 'Enter another number'
-another_number = gets.chomp
-var1 = number.to_i + another_number.to_i
-puts 'Sum of numbers = ' + var1.to_s
+puts 'Sum of numbers = ' + number.split(//).map {|x| x.to_i }.inject {|a, b| a + b}.to_s
--- 3.rb
+++ 3.rb
@@ -1,7 +1,7 @@
 puts 'Hello, what\'s your name?'
-name =  gets.chomp
+name =  gets.chomp.capitalize
 puts 'And what\'s your second name?'
-second_name = gets.chomp
+second_name = gets.chomp.capitalize
 puts 'Allright, what\'s your surname?'
-surname = gets.chomp
+surname = gets.chomp.capitalize
 puts "Greetings " + name + ' ' + second_name +' ' + surname + '!' 
